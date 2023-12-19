b9e8f1857e356d9c27f923a35d9ec16414f79949
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2023-12-19 19:06:55 +0545

    routes

--- SnapMagic/src/App.tsx
+++ SnapMagic/src/App.tsx
@@ -1,18 +1,32 @@
 import React from "react";
 import "./globals.css";
 import { Routes, Route } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
         {/* public routes */}
-        <Route
-          path="/sign-in"
-          element={<SigninForm />}
-        ></Route>
+        <Route element={<AuthLayout />}>
+          <Route
+            path="/sign-in"
+            element={<SigninForm />}
+          ></Route>
+          <Route
+            path="/sign-up"
+            element={<SignUpForm />}
+          ></Route>{" "}
+        </Route>
+
         {/* private routes */}
-        <Route index element={<Home />}></Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
       </Routes>
     </main>
   );
--- SnapMagic/src/_root/pages/Home.tsx
+++ SnapMagic/src/_root/pages/Home.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
-export default function AuthLayout() {
+const Home = () => {
   return <div></div>;
-}
+};
+
+export default Home;
