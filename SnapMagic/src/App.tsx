dd488b0865de6c55da827cce3997c28f14213f67
Yudhir <121503369+YudhirAcharya@users.noreply.github.com>
2023-12-21 04:21:08 +0545

    resetting to ts done

--- SnapMagic/src/App.tsx
+++ SnapMagic/src/App.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import "./globals.css";
+import { Route, Routes } from "react-router-dom";
+import SigninForm from "./_auth/forms/SigninForm";
+import { Home } from "./_root/pages";
+import SignupForm from "./_auth/forms/SignupForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <div>
-      <h1>hello world</h1>
-    </div>
+    <main className=" flex h-screen">
+      {/*public routes*/}
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route
+            path="/sign-in"
+            element={<SigninForm />}
+          ></Route>
+          <Route
+            path="/sign-in"
+            element={<SignupForm />}
+          ></Route>
+        </Route>
+
+        {/*private routes*/}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />}></Route>
+        </Route>
+      </Routes>
+    </main>
   );
 };
 
--- SnapMagic/src/main.tsx
+++ SnapMagic/src/main.tsx
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
+import { BrowserRouter } from "react-router-dom";
+// here ! because it is null
 ReactDOM.createRoot(
   document.getElementById("root")!
-).render(<App />);
+).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
